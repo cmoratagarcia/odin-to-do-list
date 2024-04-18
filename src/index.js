3dc8508a63aed71bb9e16c0e47a4d8e58e28bca3
Carmen Morata <cmoratagarcia@gmail.com>
2024-04-18 10:46:58 -0700

    Add New Task modal to HTML

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./style.css";
 
+const dialog = document.querySelector("dialog");
+
 function component() {
   const element = document.createElement("div");
   return element;
 }
 
 document.body.appendChild(component());
+dialog.showModal();
