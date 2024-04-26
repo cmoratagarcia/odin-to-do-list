a33535d4e3a5055b9d8831b3f90d2f12a678a8ab
Carmen Morata <cmoratagarcia@gmail.com>
2024-04-26 09:22:15 -0700

    Add functions to create DOM categories, delete and refresh the list

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,16 +1,44 @@
+import { addToLibrary, removeFromLibrary, categoryArray } from "./libraries";
+import { updateDisplay } from "../index.js";
+
 const categoryBox = document.querySelector("#new-category");
 const categories = document.querySelector(".categories");
 
-function addCategory() {
+function createCategory() {
   const newCategory = categoryBox.value;
 
-  const categoryOption = document.createElement("li");
-  categoryOption.innerText = newCategory;
-  categories.appendChild(categoryOption);
+  addToLibrary(categoryArray, newCategory);
 }
 
 categoryBox.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    addCategory();
+    createCategory();
+    refreshCategoryList();
+    categoryBox.value = "";
   }
 });
+
+function createCategoryItem(category, index) {
+  const categoryOption = document.createElement("li");
+  categoryOption.innerHTML = `${category} <i class="fa-solid fa-trash-can"></i>`;
+  categoryOption.setAttribute("id", index);
+
+  return categoryOption;
+}
+
+function refreshCategoryList() {
+  categories.innerHTML = `<li>General</li>`;
+  categoryArray.forEach((category, index) => {
+    const categoryItem = createCategoryItem(category, index);
+    categories.appendChild(categoryItem);
+  });
+
+  const deleteIcons = document.querySelectorAll(".fa-trash-can");
+  deleteIcons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+      removeFromLibrary(categoryArray, event.target.parentElement.id);
+      updateDisplay();
+    });
+  });
+}
+export { refreshCategoryList };
