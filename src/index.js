fb36966ad9f6e3af63b1906bfe53278cc599adff
Carmen Morata <cmoratagarcia@gmail.com>
2024-04-26 10:27:20 -0700

    Create separate function for deleting items

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import { createTask, refreshTaskList } from "./components/tasks.js";
 import {
   retrieveFromStorage,
+  removeFromLibrary,
   taskArray,
   categoryArray,
 } from "./components/libraries.js";
@@ -35,9 +36,17 @@ function updateDisplay() {
   refreshCategoryList();
 }
 
+function deleteItem(event, array) {
+  if (event.target.parentElement.classList.contains("delete-button")) {
+    const taskId = event.target.parentElement.id;
+    removeFromLibrary(array, taskId);
+    updateDisplay();
+  }
+}
+
 // Load tasks from localStorage when the page loads
 window.addEventListener("DOMContentLoaded", () => {
   updateDisplay();
 });
 
-export { updateDisplay };
+export { deleteItem };
