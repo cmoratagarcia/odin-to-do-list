eab68e4be3c35130c35c1369e734978c9f8cf341
Carmen Morata <cmoratagarcia@gmail.com>
2024-05-28 15:18:44 -0700

    Add validation check to Submit button

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import {
   taskArray,
   catArray,
 } from "./components/libraries.js";
-import { highlightSelectedCat, refreshCatList } from "./components/categories.js";
+import {
+  highlightSelectedCat,
+  refreshCatList,
+} from "./components/categories.js";
 
 const form = document.querySelector(".form");
 const newTask = document.querySelector(".new-task-btn");
@@ -31,8 +34,10 @@ closeDialog.addEventListener("click", () => {
 });
 
 submitBtn.addEventListener("click", () => {
-  createTask();
-  updateDisplay();
+  if (form.checkValidity()) {
+    createTask();
+    updateDisplay();
+  }
 });
 
 function updateDisplay() {
@@ -55,7 +60,7 @@ function filterTask(event) {
     (item) => item.category == event.target.innerText
   );
   refreshTaskList(filteredArray);
-    highlightSelectedCat(event)
+  highlightSelectedCat(event);
   catTitle.innerText = event.target.innerText;
 }
 
